80e6285ea32123268236fd51860c11b2b0598cb8
A-Sleepy-Fox <A.Sleepy.Fox.Mail>
2021-02-09 20:46:42 -0800

    add udonsharp sdk

---
+++ Worlds/VRC_Worlds_Shared/Assets/SleepyTestScripts/spinblocktest.cs
@@ -0,0 +1,21 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class spinblocktest : UdonSharpBehaviour
+{
+    private bool spin = false;
+
+    private void Update()
+    {
+        if(spin)
+            gameObject.transform.rotation = transform.rotation * new Quaternion(2 * Time.time, 2 * Time.time, 1 * Time.time, 1);
+    }
+
+    public override void Interact()
+    {
+        spin = !spin;
+    }
+} 
