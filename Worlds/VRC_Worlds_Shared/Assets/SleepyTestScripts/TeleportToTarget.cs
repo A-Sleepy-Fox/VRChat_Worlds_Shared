6ea89d41d7ca0147177457cfb541a533bcf5a002
Omega-Who <22parkerc@gmail.com>
2021-02-12 11:09:54 -0800

    CyanEMU implimentation and others

---
+++ Worlds/VRC_Worlds_Shared/Assets/SleepyTestScripts/TeleportToTarget.cs
@@ -0,0 +1,30 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class TeleportToTarget : UdonSharpBehaviour
+{
+    public GameObject target;
+    public bool onInteract;
+    public bool onTriggerEnter;
+
+
+    void Start()
+    {
+
+    }
+
+    public override void Interact()
+    {
+        if (onInteract)
+            Networking.LocalPlayer.TeleportTo(target.transform.position, target.transform.rotation);
+    }
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        if (onTriggerEnter)
+            Networking.LocalPlayer.TeleportTo(target.transform.position, target.transform.rotation);
+    }
+}
